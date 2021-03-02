9c61b9292f3b7f2f4043ed9aa6779604bc897de6
Charles Dale <cdale@vistaprint.com>
2021-03-01 23:24:55 -0500

    Add csv2xml

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # ap-utils
 Utilities for planning AP sorties
+
+## csv2kml
+Convert a CSV file containing photo targets into an equivalent KML file that can be imported into Google Earth or ForeFlight.
+
+Usage:
+
+```python csv2kml <filename>```
+
+The CSV file format is somewhat flexible, but it must have the following columns (names are case-insensitive):
+* Task # or Imagery ID
+* State
+* Description
+* Special Instructions (optional)
+* Start Longitude
+* Start Latitude
+
+Task # / Imagery ID is the name that will be assigned to the waypoints.  The expected format is \<STATE>_\<ID>.  If \<STATE> is not included, the value in the State column will be used.
+
+Description and Special Instructions (if specified) will be used as the Waypoint Description.
+
+Longitute and Latitude should either be decimal degrees or DD MM SS.S H, where
+* DD = Degrees
+* MM = Minutes
+* SS.S = Seconds
+* H = Hemisphere (N/S for Latitude, E/W for Longitude)
+
+### Notes
+
+See https://developers.google.com/kml/documentation for complete documentation on the KML format.  For the purposes of this program, we only care about ```Placemark``` elements.  The output KML has the following structure:
+``` XML
+<kml>
+	<Folder>
+		<Placemark>
+			<name>IDENTIFIER</name>
+			<description>DESCRIPTION/INSTRUCTIONS</description>
+			<Point>
+				<coordinates>DECIMAL_LONGITUDE,DECIMAL_LATITUDE</coordinates>
+			</Point>
+		</Placemark>
+        ...
+	</Folder>
+</kml>
+```
---
+++ csv2kml.py
@@ -0,0 +1,127 @@
+import sys
+import os
+import csv
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+
+def isfloat(value):
+  try:
+    float(value)
+    return True
+  except ValueError:
+    return False
+
+# Convert a DMS string into decimal degrees allowing either ' ' or '-' as delimiters.
+# Assume N and W hemispheres unless specified otherwise.
+def convertDMS(dms):
+    dms = dms.split(' ')
+    for item in dms:
+        if (item == ''):
+            dms.remove(item)
+    if (len(dms) == 0):
+        d = m = s = h = 0
+    elif (len(dms) == 2):
+        d, m, s = dms[0].split('-')
+        d = float(d)
+        m = float(m)
+        s = float(s)
+        h = dms[1]
+    else:
+        d, m, s, h = dms
+        d = float(d.strip("Â°" + "'" + '"'))
+        m = float(m.strip("Â°" + "'" + '"'))
+        s = float(s.strip("Â°" + "'" + '"'))
+    result = d + m/60.0 + s/3600
+    if (h == 'W') or (h == 'S'):
+        result = -result
+    return result
+
+
+# This can handle coordinates in either decimal degrees or one of a few DMS formats.
+# If lon is a positive decimal value, assume it's a typo and change it to negative (Western hemisphere)
+# If either coordinate is not a decimal value, treat it as DMS and use convertDMS to get a decimal.
+def MakeCoordinates(lon, lat):
+    if (isfloat(lon)):
+        lon = float(lon)
+        if (lon > 0):
+            print('Fixing lon: ' + str(lon))
+            lon = -lon
+    else:
+        lon = convertDMS(lon)
+    
+    if (isfloat(lat)):
+        lat = float(lat)
+    else:
+        lat = convertDMS(lat)
+
+    return str(lon) + ',' + str(lat)
+
+# This attempts to generate a standardized name using the target id and state name, accounting for observed variability in input formats
+# If the name has two or more parts, use the first two parts.  If it has only one part, prepend the value from the State column
+def MakeStandardName(name, state):
+    parts = name.split('_')
+    if (len(parts) > 1):
+        return parts[0] + '_' + parts[1]
+    return state.strip(' ') + '_' + name
+
+def ProcessFile(filename):
+    kml = ET.Element('kml', {'xmlns': 'http://www.opengis.net/kml/2.2'})
+    # The XML document uses a Folder element containing a number of Placemark elements. Here we create the Folder.
+    folder = ET.SubElement(kml, 'Folder', {
+        'xmlns:gx': 'http://www.google.com/kml/ext/2.2',
+        'xmlns:atom': 'http://www.w3.org/2005/Atom',
+        'xmlns': 'http://www.opengis.net/kml/2.2'} )
+
+    with open(filename, newline='') as targets:
+        targetreader = csv.reader(targets, delimiter=',', quotechar='|')
+        firstrow = True
+        for row in targetreader:
+            if (firstrow):
+                # Figure out which columns have the data we need
+                # This provides some flexibility on the layout of the input file as long as the columns use these names
+                colNum = 0
+                for heading in row:
+                    heading = heading.lower()
+                    if (heading == 'task #' or heading == 'imagery id'):
+                        idCol = colNum
+                    elif (heading == 'state'):
+                        stateCol = colNum
+                    elif (heading == 'description'):
+                        descCol = colNum
+                    elif (heading == 'special instructions'):
+                        instCol = colNum
+                    elif (heading == 'start longitude'):
+                        longCol = colNum
+                    elif (heading == 'start latitude'):
+                        latCol = colNum
+                    colNum += 1
+                firstrow = False
+                continue
+            # Add one Placemark for every row beyond the first.
+            # Each Placemark has this form:
+            # <Placemark>
+            #    <name>NAME</name>
+            #    <description>DESCRIPTION</description>
+            #    <Point><coordinates>LONG,LAT</coordinates></Point>
+            # </Placemark>
+            placemark = ET.SubElement(folder, 'Placemark')
+            ET.SubElement(placemark, 'name').text = MakeStandardName(row[idCol], row[stateCol])
+            description = row[descCol]
+            if (row[instCol] != ''):
+                description = description + '\n' + row[instCol]
+            ET.SubElement(placemark, 'description').text = description
+            point = ET.SubElement(placemark, 'Point')
+            ET.SubElement(point, 'coordinates').text = MakeCoordinates(row[longCol], row[latCol])
+
+    dom = minidom.parseString(ET.tostring(kml))
+    kmlfilename = os.path.splitext(filename)[0] + '.kml'
+    print('Writing KML to ' + kmlfilename)
+    kmlfile = open(kmlfilename, "wb")
+    kmlfile.write(dom.toprettyxml(encoding='UTF-8'))
+
+
+if (len(sys.argv) < 2):
+    print('Usage: python ' + sys.argv[0] + ' [csvfilename]' )
+    quit()
+
+ProcessFile(sys.argv[1]) # mawg-targets.csv
